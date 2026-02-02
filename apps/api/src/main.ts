1ef661a35548fa8c17cf283d276d544f66e93ec0
Samson <126519078+korisnik-samson@users.noreply.github.com>
2026-02-02 09:27:14 +0100

    Updated project

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,26 +10,36 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 import { PaginationDto } from '#libs/dto/pagination.dto';
 
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = 'api';
 
-  // api docs, enable swagger plugin in project.json, { "tsPlugins": ["@nestjs/swagger/plugin"] }
-  // FIXME: known issues: No parameters, @ApiHideProperty() workaround
-  const swaggerConfig = new DocumentBuilder().setTitle('Ecommerce API Docs').setVersion('0.1.0').build();
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig, { extraModels: [PaginationDto] });
-  SwaggerModule.setup('docs', app, swaggerDocument, { swaggerOptions: { docExpansion: 'none' } });
+    app.setGlobalPrefix(globalPrefix);
 
-  if (process.env.NODE_ENV !== 'production') {
-    app.enableCors();
-  }
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${ port }/${ globalPrefix }`
-  );
+    const port = process.env.PORT || 3333;
+
+    // api docs, enable swagger plugin in project.json, { "tsPlugins": ["@nestjs/swagger/plugin"] }
+    // TODO: known issues: No parameters, @ApiHideProperty() workaround
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Ecommerce API Docs')
+        .setVersion('0.1.0')
+        .build();
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig, {
+        extraModels: [PaginationDto],
+    });
+
+    SwaggerModule.setup('docs', app, swaggerDocument, {
+        swaggerOptions: { docExpansion: 'none' },
+    });
+
+    if (process.env.NODE_ENV !== 'production') app.enableCors();
+
+    await app.listen(port);
+
+    Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
 }
 
 bootstrap();
